3f028f5cb476697a3709fbd6752783aa59ef991f
Jeremy1806 <76775829+Jeremy1806@users.noreply.github.com>
2022-04-10 11:14:06 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (81.44%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1;
+        
+        while(n){
+            if(n%2){
+               ans =  n>0 ? ans*x : ans/x; 
+            }
+            x=x*x;
+            n=n/2;
+            
+        }
+        return ans;
+    }
+};
