00e08ea63e065d7730ddedf01581529104775161
Jeremy1806 <76775829+Jeremy1806@users.noreply.github.com>
2022-04-14 16:35:06 +0530

    Time: 2 ms (38.61%), Space: 6.6 MB (18.18%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-         int r1 = 0, r2 = n-1;
-        int c1 = 0, c2 = n-1;
-        int val = 0;
-		
-		// result matrix
-        vector<vector<int>> v(n, vector<int> (n));
-        while(r1 <= r2 && c1 <= c2)
-        {
-            // left to right (row will be fixed)
-            for(int i = c1; i <= c2; ++i)
-                v[r1][i] = ++val;
-				
-				// move down(col will be fixed)
-            for(int i = r1+1; i <= r2; ++i)
-                v[i][c2] = ++val;
-				
-            // move right to left
-            // move  up
-            if(r1 < r2 && c1 < c2)
-            {
-                // move right to left (row will be fixed)
-                for(int i = c2-1; i>c1; --i)
-                    v[r2][i] = ++val;
-					
-					// move up (col will be fixed)
-					for(int i = r2; i>r1; --i) 
-                    v[i][c1] = ++val;
+        
+        int h1 = 0,h2 = n-1,v1=0,v2=n-1,val=1;
+        vector<vector<int>> mat(n,vector<int>(n));
+        while(h1<=h2 and v1<=v2){
+            for(int i=h1;i<=h2;i++){
+                mat[v1][i]=val++;
             }
-            ++r1;
-            --r2;
-            ++c1;
-            --c2;
+            
+            for(int j=v1+1;j<=v2;j++){
+                mat[j][h2] = val++;
+            }
+            
+            for(int i=h2-1;i>=h1;i--){
+                mat[v2][i] = val++;
+            }
+            
+            for(int j= v2-1;j>=v1+1;j--){
+                mat[j][h1] = val++;
+            }
+            
+            h1++,v1++,v2--,h2--;
         }
-        return v;
+        return mat;
     }
 };
