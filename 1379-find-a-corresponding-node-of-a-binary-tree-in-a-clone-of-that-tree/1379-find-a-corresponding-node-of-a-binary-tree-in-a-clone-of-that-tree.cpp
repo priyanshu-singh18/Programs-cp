073182d1211c4eb9b376c87037da61989bbd0333
Jeremy1806 <76775829+Jeremy1806@users.noreply.github.com>
2022-05-17 10:49:26 +0530

    Time: 931 ms (14.13%), Space: 163.8 MB (95.67%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -10,56 +10,21 @@
 
 class Solution {
 public:
+    TreeNode* ans ;
+    
+    void trav(TreeNode* oh, TreeNode* ch, TreeNode* target){
+        if(oh){
+            if(oh == target){
+                ans = ch;
+                return; 
+            }
+            trav(oh->left,ch->left,target);
+            trav(oh->right,ch->right,target);
+        }
+    }
+    
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
-        queue<TreeNode*> qop, q;
-        qop.push(original);
-        
-        int flag = 0, count = 0;
-        
-        while(!qop.empty()){
-            int n = qop.size();
-            
-            while(n--){
-                TreeNode* node = qop.front();
-                qop.pop();
-                count++;
-                if(node == target){
-                    flag = 1;
-                    break;
-                }
-                if(node->left)
-                    qop.push(node->left);
-                if(node->right)
-                    qop.push(node->right);
-            }
-            if(flag==1)
-                break;
-        }
-        flag = 0;
-        q.push(cloned);
-        int cnt = 0;
-        TreeNode* ans;
-        
-        while(!q.empty()){
-            int n = q.size();
-            
-            while(n--){
-                TreeNode* op = q.front();
-                q.pop();
-                cnt++;
-                if(cnt == count){
-                    flag=1;
-                    ans = op;
-                    break;
-                }
-                if(op->left)
-                    q.push(op->left);
-                if(op->right)
-                    q.push(op->right);
-            }
-            if(flag == 1)
-                break;
-        }
+        trav(original,cloned, target);
         return ans;
     }
 };
