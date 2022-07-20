b4d742d81ca00769e5a352c72e91ecc2cb1db9ab
Jeremy1806 <76775829+Jeremy1806@users.noreply.github.com>
2022-07-20 18:19:41 +0530

    Time: 53 ms (18.38%), Space: 14.2 MB (41.02%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     
-    void solve(int node , int n , vector<int> vis,vector<int> v, vector<vector<int>>& graph, vector<vector<int>>& ans){
+    void solve(int node , int n,vector<int> v, vector<vector<int>>& graph, vector<vector<int>>& ans){
         
         
-        vis[node] = 1;
+    
         
         for(auto it : graph[node]){
-            if(!vis[it]){
+            // if(!vis[it]){
                 v.push_back(it);
-                solve(it,n,vis,v,graph,ans);
+                solve(it,n,v,graph,ans);
                 v.pop_back();
-            }
+            // }
         }
         if(node == n-1){
             
             ans.push_back(v);
         }
-        vis[node] = 0;
+        // vis[node] = 0;
         v.pop_back();
     }
     
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<vector<int>> ans ;
-        vector<int> vis(n,0);
-        solve(0,n,vis,{0},graph,ans);
+        // vector<int> vis(n,0);
+        solve(0,n,{0},graph,ans);
         return ans;
     }
 };
