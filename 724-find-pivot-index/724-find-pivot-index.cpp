ea41f93ac349f3211dd3192f7388f94a340a559f
Jeremy1806 <76775829+Jeremy1806@users.noreply.github.com>
2022-08-03 15:14:29 +0530

    Time: 27 ms (87.25%), Space: 31 MB (87.71%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -15,17 +15,30 @@ public:
 //                 return i;
 //         }
 //         return -1;
-        int n = nums.size();
-            vector<int> left(n,0), right(n,0);
+//         int n = nums.size();
+//             vector<int> left(n,0), right(n,0);
         
-        for(int i=1;i<n;i++)
-            left[i] = nums[i-1] + left[i-1];
-        for(int i=n-2;i>=0;i--)
-            right[i] = nums[i+1] + right[i+1];
+//         for(int i=1;i<n;i++)
+//             left[i] = nums[i-1] + left[i-1];
+//         for(int i=n-2;i>=0;i--)
+//             right[i] = nums[i+1] + right[i+1];
         
-        for(int i=0;i<n;i++){
-            if(left[i] == right[i])
+//         for(int i=0;i<n;i++){
+//             if(left[i] == right[i])
+//                 return i;
+//         }
+//         return -1;
+        
+        // o(1) space
+        
+        int leftSum = 0 , rightSum = accumulate(nums.begin(), nums.end(),0);
+        
+        for(int i=0;i<nums.size();i++){
+            rightSum-=nums[i];
+            if(leftSum == rightSum){
                 return i;
+            }
+            leftSum+=nums[i];
         }
         return -1;
     }
