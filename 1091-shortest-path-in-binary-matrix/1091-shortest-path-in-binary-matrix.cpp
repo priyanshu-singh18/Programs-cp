4dd2bf426834b044a34b63c63ecdebfa2a98f939
priyanshu-singh18 <76775829+priyanshu-singh18@users.noreply.github.com>
2026-04-14 16:25:18 +0530

    Time: 7 ms (96.37%), Space: 24.5 MB (61.65%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,50 +1,53 @@
 class Solution {
 public:
-    vector<pair<int,int>> moves = {{1,0},{0,1},{-1,0},{0,-1},{1,1},{1,-1},{-1,-1},{-1,1}};
-    
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        
-        if(grid[0][0]!=0 || grid[n-1][n-1]!=0){
+
+        if (grid[0][0] == 1 || grid[n-1][n-1] == 1){
             return -1;
         }
-        
-        queue<pair<int,int>> qop;
-        qop.push({0,0});
-        int step = 0;
-        
-        while(!qop.empty()){
-            int size = qop.size();
-            step++;
-            while(size--){
-                int x = qop.front().first;
-                int y = qop.front().second;
-                qop.pop();
-            
-                 
-                if(x == n-1 and y == n-1)
-                    return step;
-                    
-                
-                for(auto it : moves){
-                    int x1 = x + it.first;
-                    int y1 = y + it.second;
-                
-                
-                    if(x1<0 or y1<0 or x1>n-1 or y1>n-1 or grid[x1][y1]!=0)
-                        continue;
-                
-                    grid[x1][y1] = 1;
-                    qop.push({x1,y1});
-                
-                }
-            }
-            
-    
+
+        if ( n == 1){
+            return 1;
         }
-        return -1;
-    
-    
-   
+
+        vector<pair<int,int>> directions = {
+            {0,1} , {0,-1} , {1, 0} , {-1 , 0},
+            {1, 1} , {1, -1} , {-1 , 1} , {-1, -1}
+        };
+
+        queue<tuple<int,int,int>>  q;
+
+        q.push({0,0,1});
+
+        grid[0][0] = 1;
+
+        while(!q.empty()){
+            auto [row, col ,pathLen] = q.front();
+
+            q.pop();
+
+            for (auto [dr  , dc] : directions){
+                int newRow = row + dr;
+                int newCol = col + dc;
+
+                if(newRow < 0 || newRow>=n || newCol < 0 || newCol>= n){
+                    continue;
+                }
+
+                if(grid[newRow][newCol] != 0){
+                    continue;
+                }
+
+                if(newRow == n-1 && newCol == n-1){
+                    return pathLen + 1;
+                }
+
+                grid[newRow][newCol] = 1;
+                q.push({newRow , newCol , pathLen + 1});
+            }
+
+        }
+        return -1 ;
     }
 };
